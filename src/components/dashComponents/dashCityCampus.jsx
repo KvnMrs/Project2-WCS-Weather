c246aff16c6df4abaf73547c89f928d251ebb766
François Chatelier <francois.chatelier@gmail.com>
2021-11-04 19:42:52 +0100

    Mise en place dashboard Campus villes

---
+++ src/components/dashComponents/dashCityCampus.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function DashCity() {
+  return (
+    <div className="rounded-lg bg-blue-300 h-100 mb-5">
+      <div className="grid grid-cols-3">
+        <div>
+          <h1>
+            Lisbonne
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default DashCity;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@
 /* eslint-disable react/button-has-type */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
+import DashCity from '../components/dashComponents/dashCityCampus';
 import DashAirQuality from '../components/dashComponents/dashPrincipalAir';
 import DashMeteo from '../components/dashComponents/dashPrincipalMeteo';
 import GraphiqueAir from '../components/dashComponents/graphiqueAir';
@@ -185,7 +186,7 @@ function Dash() {
               <h1 className="text-lg text-neutral-600">France</h1>
                 </div>
                 </section>
-                <section className="sm:m-2 p-2 row grid-cols gap-y-4 lg:grid grid-cols-2 gap-4">
+                <section className="m-2 p-2 row grid-cols md:grid grid-cols-2 gap-4">
                 <div>
                 <DashAirQuality />
                 </div>
@@ -199,9 +200,27 @@ function Dash() {
                     <GraphiqueMeteo />
                 </div>
                 </section>
+              {/* CONTENT HERE */}
                 </div>
               </div>
-              {/* CONTENT HERE */}
+               {/* DASHBOARD CAMPUS VILLES */}
+               <div className="py-4 pt-4 mt-6">
+                <div className="rounded-lg bg-gray-50 h-110">
+                <section className="pl-4 pt-8">
+                <h1 className="text-3xl font-semibold leading-none tracking-tighter text-neutral-600">
+                  WCS CAMPUS
+                </h1>
+                <div className="px-4 max-w-7xl sm:px-6 md:px-8">
+              <h1 className="text-lg text-neutral-600">in Nantes, France</h1>
+                </div>
+                </section>
+                <section className="m-2 p-2 row grid-cols md:grid grid-cols-2 gap-4">
+                <div>
+                <DashCity />
+                </div>
+                </section>
+                </div>
+               </div>
             </div>
           </div>
         </main>
