e41aa59c4c6d08027a3911723610b8ec493f15c7
François Chatelier <francois.chatelier@gmail.com>
2021-11-06 12:47:17 +0100

    Mise en forme + icone navbar  desktop

--- src/components/navigation_Desktop/navbarDesktop.jsx
+++ src/components/navigation_Desktop/navbarDesktop.jsx
@@ -62,15 +62,16 @@ function NavBarDesktop() {
                       ease-in-out
                       transform
                       rounded-lg
-                      bg-gray-50
+                      bg-wild_red
                       focus:shadow-outline
+                      hover:bg-dark_wild_red
                     "
                     href="#"
                   >
-                    <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
                     </svg>
-                    <span className="ml-4"> Overview</span>
+                    <span className="ml-4">Dashboard</span>
                   </a>
                 </li>
                 <li>
@@ -89,14 +90,16 @@ function NavBarDesktop() {
                       transform
                       rounded-lg
                       focus:shadow-outline
-                      hover:bg-gray-50
+                      hover:bg-gray-100
                     "
                     href="#"
                   >
-                    <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <path d="M12 14l9-5-9-5-9 5 9 5z" />
+                      <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
+                      <path strokelinecap="round" strokelinejoin="round" strokewidth="2" d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222" />
                     </svg>
-                    <span className="ml-4">Chat</span>
+                    <span className="ml-4">WCS Campus</span>
                   </a>
                 </li>
                 <li>
@@ -115,41 +118,40 @@ function NavBarDesktop() {
                       transform
                       rounded-lg
                       focus:shadow-outline
-                      hover:bg-gray-50
+                      hover:bg-gray-100
                     "
                     href="#"
                   >
-                    <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <path strokelinecap="round" strokeLinejoin="round" strokewidth="2" d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
+                    <span className="ml-4">European Cities</span>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="
+                      inline-flex
+                      items-center
+                      w-full
+                      px-4
+                      py-2
+                      mt-1
+                      text-base text-gray-900
+                      transition
+                      duration-500
+                      ease-in-out
+                      transform
+                      rounded-lg
+                      focus:shadow-outline
+                      hover:bg-gray-100
+                    "
+                    href="#"
+                  >
+                    <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
                     </svg>
-                    <span className="ml-4">User</span>
-                  </a>
-                </li>
-                <li>
-                  <a
-                    className="
-                      inline-flex
-                      items-center
-                      w-full
-                      px-4
-                      py-2
-                      mt-1
-                      text-base text-gray-900
-                      transition
-                      duration-500
-                      ease-in-out
-                      transform
-                      rounded-lg
-                      focus:shadow-outline
-                      hover:bg-gray-50
-                    "
-                    href="#"
-                  >
-                    <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
-                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                    </svg>
-                    <span className="ml-4">Settings</span>
+                    <span className="ml-4">About</span>
 
                   </a>
                 </li>
