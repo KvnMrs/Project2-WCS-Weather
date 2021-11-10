95c2a324d1174e38815739d5bf52201b2205ab31
Camille Hautois <camille.hautois@gmail.com>
2021-11-10 16:28:50 +0100

    Update currentWeatherCard css

--- src/components/dashComponents/DashboardCard.jsx
+++ src/components/dashComponents/DashboardCard.jsx
@@ -7,18 +7,18 @@ import React from 'react';
 
 export const CurrentWeatherCard = ({ weather, temperature }) => {
   return (
-    <div className="grid grid-cols-3 grid-rows-1 gap-4">
-      <div className="justify-self-center grid-rows-2 sm:pt-8">
-        <h1 className="text-xl md:text-3xl text-center">
-          {weather.description}
-          <img src={'https://openweathermap.org/img/wn/' + weather.icon + '@2x.png'} alt="weather icon" />
-          <br />
-        </h1>
-      </div>
+    <div className="grid grid-rows-1 gap-4">
       <div className="justify-self-center self-center ml-9 sm:pt-4">
         <div className="sm: grid grid-row-span-3 mt-2">
-          <h2 className="text-3xl md:text-4xl">{Math.round(temperature.temp)} °C</h2>
+          <h1 className="text-xl md:text-3xl text-center">
+            {weather.description}
+          </h1>
+          <div className="justify-self-center grid-rows-2 sm:pt-4">
+            <img src={'https://openweathermap.org/img/wn/' + weather.icon + '@2x.png'} alt="weather icon" />
+          </div>
+          <h2 className="whitespace-nowrap text-center text-3xl md:text-4xl">{Math.round(temperature.temp)} °C</h2>
         </div>
+        <br />
       </div>
     </div>
   );
