3712e979373bc760e3d94f730ffbea0e9850bf6d
Kévin Marais <86494393+KvnMrs@users.noreply.github.com>
2021-11-17 15:17:29 +0100

    importation barna - un peu de nettoyage

--- src/components/welcomeComponents/UserWelcomemsg.jsx
+++ src/components/welcomeComponents/UserWelcomemsg.jsx
@@ -5,7 +5,7 @@ const UserWelcomemsg = () => {
   return (
     <div>
       <section>
-        <h1 className=" text-center mt-9 sm:mt-0 sm:text-left text-3xl font-semibold leading-none tracking-tighter text-neutral-600 pl-2 pt-5">
+        <h1 className=" text-center mt-12 sm:mt-0 sm:text-left text-3xl font-semibold leading-none tracking-tighter text-neutral-600 pl-2 pt-5">
           Hello, Wilder !
         </h1>
       </section>
