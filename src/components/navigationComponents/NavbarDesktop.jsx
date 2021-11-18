122d310dff200cab0fcd16c0170d60013c1aea6b
Kévin Marais <86494393+KvnMrs@users.noreply.github.com>
2021-11-18 12:11:14 +0100

    photos lien ok

--- src/components/navigationComponents/NavbarDesktop.jsx
+++ src/components/navigationComponents/NavbarDesktop.jsx
@@ -111,46 +111,6 @@ function NavBarDesktop() {
                       focus:shadow-outline
                       hover:bg-wild_red
                     "
-                    href="/dashboard"
-                  >
-                    {/** LINK FOR CAMPUS CHOOSEN BY USER */}
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-5 w-5"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path d="M12 14l9-5-9-5-9 5 9 5z" />
-                      <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
-                      <path
-                        strokelinecap="round"
-                        strokelinejoin="round"
-                        strokewidth="2"
-                        d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-                      />
-                    </svg>
-                    <span className="ml-4">WCS Campus</span>
-                  </a>
-                </li>
-                <li>
-                  <a
-                    className="
-                      inline-flex
-                      items-center
-                      w-full
-                      px-4
-                      py-2
-                      mt-1
-                      text-base text-gray-900
-                      transition
-                      duration-500
-                      ease-in-out
-                      transform
-                      rounded-lg
-                      focus:shadow-outline
-                      hover:bg-wild_red
-                    "
                     href="/europeancity"
                   >
                     {/** LINK FOR ALL EUROPEEAN CITIES */}
